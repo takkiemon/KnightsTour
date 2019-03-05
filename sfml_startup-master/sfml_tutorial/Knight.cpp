26c9d0a4b11c759f3697d8870f5f7e8f1929ab61
takkiemon <takkie91@hotmail.com>
2019-03-05 20:12:21 +0100

    add project

---
+++ sfml_startup-master/sfml_tutorial/Knight.cpp
@@ -0,0 +1,10 @@
+#include "Knight.h";
+
+void Knight::Update(Time deltaTime)
+{
+	printf("Knight::Update\n");
+	//call the base class's Update function
+	SpriteRenderer::Update(deltaTime);
+
+	//update the knight's position
+}
